71edef8fe06fe89452fe1d1de7ffb60a694c181f
Matt Morgan <mmorgan@cloudbolt.io>
2023-07-11 14:26:22 -0700

    add all types to var_map_test

--- var_map_test/main.tf
+++ var_map_test/main.tf
@@ -1,11 +1,45 @@
 variable "map_type_test" {
-    type = map(any)
-    default = {name = "Mabel", content = 52}
+  type = object({ name : string, content : number })
+  default = {
+    name    = "Mabel",
+    content = 52
+  }
+}
+
+variable "list_type_test" {
+  type    = list(any)
+  default = ["red", "yellow", "green"]
+}
+
+variable "bool_type_test" {
+  type    = bool
+  default = false
 }
 
 
 resource "local_file" "foo" {
-    content     = var.map_type_test["content"]
-    filename = "/tmp/${var.map_type_test["name"]}"
+  filename = "/tmp/${var.map_type_test["name"]}"
+  content  = <<-EOT
+    ${var.map_type_test["content"]}
+    %{for color in var.list_type_test~}
+list item = ${color}
+    %{endfor~}
+Bool vaule = ${var.bool_type_test}
+  EOT
+
+
 }
+
+output "my_map" {
+  value = var.map_type_test
+}
+
+output "my_list" {
+  value = var.list_type_test
+}
+
+output "my_bool" {
+  value = var.bool_type_test
+}
+
 
