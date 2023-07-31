8374b66d18081e815684e096ece2de89921ff44e
Matt Morgan <mmorgan@cloudbolt.io>
2023-07-31 11:03:09 -0700

    update conn info

--- pg_backend/main.tf
+++ pg_backend/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "pg" {
-    conn_str = "postgres://cb_dba:cb_pass@localhost/terraform_backend?sslmode=disable"
+    conn_str = "postgres://cb_dba:cloudbolt@localhost/terraform_backend?sslmode=disable"
   }
 }
 
@@ -11,4 +11,8 @@ variable "content" {
 resource "local_file" "config_file" {
     content     = var.content
     filename = "/tmp/config_a.txt"
+}
+
+output "some_thing" {
+  value = "blarg!"
 }
